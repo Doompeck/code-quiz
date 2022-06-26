26067ec405ef586839bec8d5d9fada2cbf5f3484
Benjamin <bstanf@uw.edu>
2022-06-26 13:58:25 -0700

    adding in comments and starting to set out structure for html template literals

---
+++ assets/script/script.js
@@ -0,0 +1,27 @@
+var timeEl;
+// Question and Answer Array
+var javaQuestions = [
+   //Initial object structure
+    {
+        question: "",
+        answers:{
+            a: "",
+            b: "",
+            c: ""
+        },
+        correctAnswer: ""
+    },
+
+];
+
+
+
+
+
+
+
+// Quiz function
+function quizElement() {
+
+
+}
