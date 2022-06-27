292344e9b1db439d2cce5433aab195dcbcb7b596
Benjamin <bstanf@uw.edu>
2022-06-26 18:09:48 -0700

    Adding more structure, logic, variables to the js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,141 +1,137 @@
 //  selecting html elements
-var question = document.getElementById("question");
-var choiceA = document.getElementById("a");
-var choiceB = document.getElementById("b");
-var choiceC = document.getElementById("c");
-var choiceD = document.getElementById("d");
+var question = document.getElementById("#question");
+var choiceA = document.getElementById("#a");
+var choiceB = document.getElementById("#b");
+var choiceC = document.getElementById("#c");
+var choiceD = document.getElementById("#d");
 var startQuiz = document.getElementById("start");
 
+// Variables
+var timeCountdown = 75;
+var questionPosition = 0;
+
 // 10 Questions and  4 Answer Array w correct answers
-var javaQuestions = [
-   //Initial object structure
+var questions = [
     {
         question: "Javascript is an _______ language?",
-        answers:{
-            a: "Object-Oriented",
-            b: "Object-Based",
-            c: "Procedural",
-            d: "None of the Above"
-        },
-        correctAnswer: "a"
+        answer:["Object-Oriented","Object-Based","Procedural","None of the Above"],
+        correctAnswer: "Object-Oriented"
     },
     {
         question: "Which of the following keywords is used to define a variable in Javascript?",
-        answers:{
-            a: "var",
-            b: "let",
-            c: "Both A and B",
-            d: "None of the Above"
-        },
-        correctAnswer: "c"
+        answer:["var","let","Both A and B","None of the Above"],
+        correctAnswer: "Both A and B"
     },
     {
         question: "Which of the following methods is used to access HTML elements using Javascript?",
-        answers:{
-            a: "getElementById()",
-            b: "getElementsbyClassName()",
-            c: "Both A and B",
-            d: "None of the Above"
-        },
-        correctAnswer: "c"
+        answer:["getElementById()","getElementsbyClassName()","Both A and B","None of the Above"],
+        correctAnswer: "Both A and B"
     },
     {
         question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
-        answers:{
-            a: "Throws and error",
-            b: "Ignores the statements",
-            c: "Gives a warning",
-            d: "None of the Above"
-        },
-        correctAnswer: "b"
+        answer:["Throws and error","Ignores the statements","Gives a warning","None of the Above"],
+        correctAnswer: "Ignores the statements"
     },
     {
         question: "Which of the following methods can be used to display data in some form using Javascript?",
-        answers:{
-            a: "document.write()",
-            b: "console.log()",
-            c: "window.alert()",
-            d: "All of the Above"
-        },
-        correctAnswer: "d"
+        answer:["document.write()","console.log()","window.alert()","All of the Above"],
+        correctAnswer: "All of the Above"
     },
     {
         question: "How can a datatype be declared to be a constant type?",
-        answers:{
-            a: "const",
-            b: "var",
-            c: "let",
-            d: "constant"
-        },
-        correctAnswer: "a"
+        answer:["const","var","let","constant"],
+        correctAnswer: "const"
     },
     {
         question: "When an operator's value is NULL, the typeof returned by the unary operator is:",
-        answers:{
-            a: "Boolean",
-            b: "Undefined",
-            c: "Object",
-            d: "Integer"
-        },
-        correctAnswer: "c"
+        answer:["Boolean","Undefined","Object","Integer"],
+        correctAnswer: "Object"
     },
     {
         question: "When the switch statement matches the expression with the given labels, how is the comparison done?",
-        answers:{
-            a: "Both the datatype and the result of the expression are compared",
-            b: "Only the datatype  of the expression is compared.",
-            c: "Only the value of the expression is compared.",
-            d: "None of the Above"
-        },
-        correctAnswer: "a"
+        answer:["Both the datatype and the result of the expression are compared","Only the datatype  of the expression is compared.","Only the value of the expression is compared.","None of the Above"],
+        correctAnswer: "Both the datatype and the result of the expression are compared"
     },
     {
         question: "What does the Javascript “debugger” statement do?",
-        answers:{
-            a: "It will debug all the errors in the program at runtime",
-            b: "It acts as a breakpoint in a program.",
-            c: "It will debug error in the current statement if any.",
-            d: "All of the Above"
-        },
-        correctAnswer: "b"
+        answer:["It will debug all the errors in the program at runtime","It acts as a breakpoint in a program.","It will debug error in the current statement if any.","All of the Above"],
+        correctAnswer: "It acts as a breakpoint in a program."
     },
     {
         question: "What keyword is used to check whether a given property is valid or not?",
-        answers:{
-            a: "in",
-            b: "is in",
-            c: "exists",
-            d: "lies"
-        },
-        correctAnswer: "a"
+        answer:["in","is in","exists","lies"],
+        correctAnswer: "in"
     }
 ];
 
-// Variables
-var lastQuestion = javaQuestions.length - 1;
-// 90 second timer
-var timeCountdown = 90;
+var currentQuestion = questions[questionPosition];
+var questionContainer = document.querySelector('#question');
 
-function starTimer(){
-var timer = setInterval(function() {
-    timeCountdown--;
-    document.getElementById("timer").textContent = timeCountdown;
-    if(timeCountdown <= 0)
-    clearInterval(timer);
-}, 1000);
-}
+questionContainer.addEventListener("click", function(event) {
+
+    var clickElement = event.target;
+    
+    if(!clickElement.matches('button')) return;
+
+    var answer = clickElement.dataset.answer;
+    console.log(answer);
+
+});
+
+startQuiz.addEventListener("click", function(event) {
+
+    var clickElement = event.target;
+    
+    if(!clickElement.matches('button')) return;
+   
+    startGame();
+
+});
+
+
+
+
+
 
 // Quiz function
-function quizElement() {
-
-}
-
-// Correct Answer
-function correctAnswer(){
+function startGame() {
+    //Need to Hide start screen
     
+    // Display current question
+
+    
+    startTimer();
+    
+    // Answer Question
+    answerQuestion();
+        
+// Display next question
+questionPosition++
+displayCurrentQuestion();
 }
 
-// Wrong Answer
-function wrongAnswer(){
-
+function answerQuestion(){
+    var currentQuestion = questions[questionPosition];
+    // Validate Answer, if wrong then subtract from timeCountdown
+    questionPosition++
 }
+
+
+
+
+
+    //  Display current question
+// HTML Literal will go here for question and answers
+function displayCurrentQuestion(){
+    questions[questionPosition]
+}
+
+// 75 second timer
+function startTimer(){
+    var timer = setInterval(function() {
+        timeCountdown--;
+        document.getElementById("timer").textContent = timeCountdown;
+        if(timeCountdown <= 0)
+        clearInterval(timer);
+    }, 1000);
+    }
